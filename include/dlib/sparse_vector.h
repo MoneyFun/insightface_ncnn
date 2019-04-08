f3133f29fda947f4d882a21a4a85914bcd0776f9
1012041750@qq.com <1012041750@qq.com>
2019-04-08 15:53:53 +0800

    dlib

---
+++ include/dlib/sparse_vector.h
@@ -0,0 +1,8 @@
+// Copyright (C) 2012  Davis E. King (davis@dlib.net)
+// License: Boost Software License   See LICENSE.txt for the full license.
+#ifndef DLIB_SPaRSE_VECTOR_Hh_
+#define DLIB_SPaRSE_VECTOR_Hh_
+
+#endif // DLIB_SPaRSE_VECTOR_Hh_
+
+
