255e913d77090d043deabeeee2b1a9d4f2026380
1012041750@qq.com <1012041750@qq.com>
2019-04-09 15:17:02 +0800

    eye open detect

--- arcface/main.cpp
+++ arcface/main.cpp
@@ -12,73 +12,6 @@ using namespace std;
 
 #define FACE_COUNT 5
 
-double K[9] = { 6.5308391993466671e+002, 0.0, 3.1950000000000000e+002, 0.0, 6.5308391993466671e+002, 2.3950000000000000e+002, 0.0, 0.0, 1.0 };
-double D[5] = { 7.0834633684407095e-002, 6.9140193737175351e-002, 0.0, 0.0, -1.3073460323689292e+000 };
-
-Mat calcAngle(dlib::full_object_detection shape) {
-    std::vector<cv::Point2d> image_pts;
-
-    //fill in cam intrinsics and distortion coefficients
-    cv::Mat cam_matrix = cv::Mat(3, 3, CV_64FC1, K);
-    cv::Mat dist_coeffs = cv::Mat(5, 1, CV_64FC1, D);
-
-    //fill in 3D ref points(world coordinates), model referenced from http://aifi.isr.uc.pt/Downloads/OpenGL/glAnthropometric3DModel.cpp
-    std::vector<cv::Point3d> object_pts;
-    object_pts.push_back(cv::Point3d(6.825897, 6.760612, 4.402142));     //#33 left brow left corner
-    object_pts.push_back(cv::Point3d(1.330353, 7.122144, 6.903745));     //#29 left brow right corner
-    object_pts.push_back(cv::Point3d(-1.330353, 7.122144, 6.903745));    //#34 right brow left corner
-    object_pts.push_back(cv::Point3d(-6.825897, 6.760612, 4.402142));    //#38 right brow right corner
-    object_pts.push_back(cv::Point3d(5.311432, 5.485328, 3.987654));     //#13 left eye left corner
-    object_pts.push_back(cv::Point3d(1.789930, 5.393625, 4.413414));     //#17 left eye right corner
-    object_pts.push_back(cv::Point3d(-1.789930, 5.393625, 4.413414));    //#25 right eye left corner
-    object_pts.push_back(cv::Point3d(-5.311432, 5.485328, 3.987654));    //#21 right eye right corner
-    object_pts.push_back(cv::Point3d(2.005628, 1.409845, 6.165652));     //#55 nose left corner
-    object_pts.push_back(cv::Point3d(-2.005628, 1.409845, 6.165652));    //#49 nose right corner
-    object_pts.push_back(cv::Point3d(2.774015, -2.080775, 5.048531));    //#43 mouth left corner
-    object_pts.push_back(cv::Point3d(-2.774015, -2.080775, 5.048531));   //#39 mouth right corner
-    object_pts.push_back(cv::Point3d(0.000000, -3.116408, 6.097667));    //#45 mouth central bottom corner
-    object_pts.push_back(cv::Point3d(0.000000, -7.415691, 4.070434));    //#6 chin corner
-
-    //result
-    cv::Mat rotation_vec;                           //3 x 1
-    cv::Mat rotation_mat;                           //3 x 3 R
-    cv::Mat translation_vec;                        //3 x 1 T
-    cv::Mat pose_mat = cv::Mat(3, 4, CV_64FC1);     //3 x 4 R | T
-    cv::Mat euler_angle = cv::Mat(3, 1, CV_64FC1);
-
-    //temp buf for decomposeProjectionMatrix()
-    cv::Mat out_intrinsics = cv::Mat(3, 3, CV_64FC1);
-    cv::Mat out_rotation = cv::Mat(3, 3, CV_64FC1);
-    cv::Mat out_translation = cv::Mat(3, 1, CV_64FC1);
-
-
-    //fill in 2D ref points, annotations follow https://ibug.doc.ic.ac.uk/resources/300-W/
-    image_pts.push_back(cv::Point2d(shape.part(17).x(), shape.part(17).y())); //#17 left brow left corner
-    image_pts.push_back(cv::Point2d(shape.part(21).x(), shape.part(21).y())); //#21 left brow right corner
-    image_pts.push_back(cv::Point2d(shape.part(22).x(), shape.part(22).y())); //#22 right brow left corner
-    image_pts.push_back(cv::Point2d(shape.part(26).x(), shape.part(26).y())); //#26 right brow right corner
-    image_pts.push_back(cv::Point2d(shape.part(36).x(), shape.part(36).y())); //#36 left eye left corner
-    image_pts.push_back(cv::Point2d(shape.part(39).x(), shape.part(39).y())); //#39 left eye right corner
-    image_pts.push_back(cv::Point2d(shape.part(42).x(), shape.part(42).y())); //#42 right eye left corner
-    image_pts.push_back(cv::Point2d(shape.part(45).x(), shape.part(45).y())); //#45 right eye right corner
-    image_pts.push_back(cv::Point2d(shape.part(31).x(), shape.part(31).y())); //#31 nose left corner
-    image_pts.push_back(cv::Point2d(shape.part(35).x(), shape.part(35).y())); //#35 nose right corner
-    image_pts.push_back(cv::Point2d(shape.part(48).x(), shape.part(48).y())); //#48 mouth left corner
-    image_pts.push_back(cv::Point2d(shape.part(54).x(), shape.part(54).y())); //#54 mouth right corner
-    image_pts.push_back(cv::Point2d(shape.part(57).x(), shape.part(57).y())); //#57 mouth central bottom corner
-    image_pts.push_back(cv::Point2d(shape.part(8).x(), shape.part(8).y()));   //#8 chin corner
-
-    //calc pose
-    cv::solvePnP(object_pts, image_pts, cam_matrix, dist_coeffs, rotation_vec, translation_vec);
-
-    //calc euler angle
-    cv::Rodrigues(rotation_vec, rotation_mat);
-    cv::hconcat(rotation_mat, translation_vec, pose_mat);
-    cv::decomposeProjectionMatrix(pose_mat, out_intrinsics, out_rotation, out_translation, cv::noArray(), cv::noArray(), cv::noArray(), euler_angle);
-
-    return euler_angle;
-}
-
 cv::Mat ncnn2cv(ncnn::Mat img)
 {
     unsigned char pix[img.h * img.w * 3];
@@ -96,6 +29,13 @@ cv::Mat ncnn2cv(ncnn::Mat img)
     return cv_img;
 }
 
+float calcY(int left, int center, int right){
+    float left_width = center - left;
+    float right_width = right - center;
+
+    return ((left_width - right_width) * 10/(right - left));
+}
+
 #define ANGLE_DIMENSION 3
 
 int main(int argc, char* argv[])
@@ -109,6 +49,8 @@ int main(int argc, char* argv[])
     //3 X,Y,Z
     float angle[ANGLE_DIMENSION] = {0};
     float mouth_open_level = 0;
+    float yAngle = 0;
+    float eye[2] = {0};
 
     MtcnnDetector detector("../models");
 
@@ -160,32 +102,47 @@ int main(int argc, char* argv[])
 
             detect_result = pose_model(cimg, face);
 #if 0
-            for (int j = 0; j < detect_result.num_parts(); j++) {
+            for (int j = 36; j < 42; j++) {
+                p = detect_result.part(j);
+                cv::circle(frame, cv::Point(p.x(), p.y()), 3, cv::Scalar(255,0,0), -1);
+                cout << j << ":" << p.x() << endl;
+            }
+
+            for (int j = 42; j < 48; j++) {
                 p = detect_result.part(j);
                 cv::circle(frame, cv::Point(p.x(), p.y()), 3, cv::Scalar(255,0,0), -1);
                 cout << j << ":" << p.x() << endl;
             }
 #endif
 
-
             mouth_open_level = mouth_open_level * 0.9 + abs(0.1 * (detect_result.part(57).y() - detect_result.part(51).y())/
                 (detect_result.part(54).x() - detect_result.part(48).x()));
 
-            outtext << std::setprecision(2) << mouth_open_level;
+            outtext << "mouth: " << std::setprecision(2) << mouth_open_level;
             cv::putText(frame, outtext.str(), cv::Point(50, 100), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
             outtext.str("");
 
-            Mat euler_angle = calcAngle(detect_result);
-            for(int i = 0; i < ANGLE_DIMENSION; i++)
-                angle[i] = angle[i] * 0.95 + 0.05 * euler_angle.at<double>(i);
+            float currentY = calcY(detect_result.part(0).x(), detect_result.part(27).x(), detect_result.part(16).x());
+            // cout << "currentY: " << currentY << endl;
+            yAngle = yAngle * 0.9 + 0.1 * currentY;
 
-            int x = 50;
-            int y = 60;
-            for(int i = 0; i < ANGLE_DIMENSION; i++) {
-                outtext << std::setprecision(2) << angle[i];
-                cv::putText(frame, outtext.str(), cv::Point(50, 40 + 20 * i), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
-                outtext.str("");
-            }
+            outtext << std::setprecision(2) << yAngle;
+            cv::putText(frame, outtext.str(), cv::Point(50, 40), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
+            outtext.str("");
+
+            eye[0] = eye[0] * 0.9 + abs(0.1 * (detect_result.part(39).x() - detect_result.part(36).x()) /
+                (detect_result.part(41).y() - detect_result.part(37).y()));
+
+            outtext << "eye0: " << std::setprecision(2) << eye[0];
+            cv::putText(frame, outtext.str(), cv::Point(50, 60), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
+            outtext.str("");
+
+            eye[1] = eye[1] * 0.9 + abs(0.1 * (detect_result.part(45).x() - detect_result.part(42).x()) /
+                (detect_result.part(43).y() - detect_result.part(47).y()));
+
+            outtext << "eye1: " << std::setprecision(2) << eye[1];
+            cv::putText(frame, outtext.str(), cv::Point(50, 80), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 0));
+            outtext.str("");
         }
         imshow("camera", frame);
 
